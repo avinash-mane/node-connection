3318c9e50cc89884cdfbcfbbc50dd6d77e6e83a3
Avinash Mane <59219401+avinash-mane@users.noreply.github.com>
2021-08-24 22:36:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,38 @@
 
+const express = require("express")
+const app = express();
+app.use(express.json())
+const cors = require("cors")
+app.use(cors())
+
+app.post("/user", (req, res) => {
+    console.log(req.body)
+    res.send({ name: "a" })
+
+})
+app.get("/user", (req, res) => {
+    res.send("get")
+})
+app.listen(3210, () => console.log("server started"))
+
+//api call
+// axios.get("http://localhost:3210/user").then(() => { })
+// axios.post("http://localhost:3210/user", { data: "avinash" }).then(() => { })
+
+//fordb connection
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  con.query("create database db")
+  console.log("Connected!");
+});
+
+//if we write conection on seprate file
+// module.exports=con;
